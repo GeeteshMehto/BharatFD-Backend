a0434329ffc9f111573ea68cf7fb41f6dfb6149d
GeeteshMehto <geeteshmehto018@gmail.com>
2025-02-02 01:51:59 +0530

    Edited build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e  # Exit immediately if a command exits with a non-zero status
 echo "🚀 BUILD START"
 
 # Install SQLite dependencies
-apk update && apk add --no-cache sqlite-libs
+sudo apt update && sudo apt install --no-cache sqlite3
 
 # Install Python dependencies
 python3 -m pip install --upgrade pip
