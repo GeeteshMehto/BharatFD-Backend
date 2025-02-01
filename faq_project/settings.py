773be36d71f3c35caffd61cf7f9031909384cb4f
GeeteshMehto <geeteshmehto018@gmail.com>
2025-02-02 01:07:43 +0530

    Edited wsgi.py

--- faq_project/settings.py
+++ faq_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+wwcjckgn@1%4(y8yv)$d4i13tq%g67z(9di2+d7=r_ei)84*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
